760752ccee91815dc2bb01b9eae7d5c06469ed53
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2024-07-14 14:45:32 +0100

    feat: setup auth module

---
+++ api/auth/auth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+class Authenticator {
+  accessTokenSecret;
+
+  constructor(accessTokenSecret) {
+    this.accessTokenSecret = accessTokenSecret;
+  }
+
+  static authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) {
+      return res.sendStatus(401);
+    }
+
+    jwt.verify(token, this.accessTokenSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  }
+}
+
+module.exports = Authenticator;
