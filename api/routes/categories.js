04d0d6b2a7e58fb09135fdf882c498662bca4709
Mayowa <88011448+Maryor-z@users.noreply.github.com>
2024-07-22 16:20:44 +0100

    feat:setup categoryRoute

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,23 +1,13 @@
 const router = require("express").Router();
 const Category = require("../models/Category");
+const Categories = require("../controllers/CategoriesController");
+const CategoriesController = require("../controllers/CategoriesController");
+
+const { newCategory, findCategory} = CategoriesController;
 
 
-router.post("/", async (req,res) => {
-    const newCat = new Category(req.body);
-    try {
-        const savedCat = await newCat.save();
-        res.status(200).json(savedCat);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.post("/", newCategory);
 
-router.get("/", async (req,res) => {
-    try {
-        const cats = await Category.find();
-        res.status(200).json(cats);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.get("/", findCategory);
+
 module.exports = router;
